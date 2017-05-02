0fde4872d51a4c42552e5aa0284847977f1a3714
Matthew Skay <skaymj@gmail.com>
2017-05-02 17:10:42 -0400

    Update song.js

--- final-project/song.js
+++ final-project/song.js
@@ -1,8 +1,5 @@
 jQuery(document).ready(function(){
-
-	var url = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/285701757&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
-	$("#videoPlayer").attr("src", url);
-
+	
 	$('#thumbsUp').click(function() { 
 		var text = parseInt($('#numLike').text());
 		var newText = text + 1;
@@ -27,11 +24,5 @@ jQuery(document).ready(function(){
 			$('#numLike').text(newText);
 			$('#thumbsUp').removeClass('inactiveLink');
 		}
-	});
-
-	$('#commentButton').click(function() { 
-		var comment = $('#addComment').val();
-		$('#commentSection').append('<hr><h5>Username</h5><p>'+comment+'</p>');
-		$('#addComment').val('');
 	});
 });
