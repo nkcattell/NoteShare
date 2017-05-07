060fc3e55fa4b2d79a6f84471a5ae6e69e98133f
Matthew Skay <skaymj@gmail.com>
2017-05-07 18:48:42 -0400

    Improve database access

--- final-project/song.php
+++ final-project/song.php
@@ -1,17 +1,19 @@
 <?php
-	
-	$url = "https://www.youtube.com/embed/HdzI-191xhU";
+	require_once "userAuth.php";
+
+	$url = "https://www.youtube.com/embed/FM7MFYoylVs";
+
 	// Change this
 	$username = "Username";
 
 	/* Connecting to the database */		
-	$db_connection = new mysqli('localhost', 'root', '', 'notesharedb');
+	$db_connection = new mysqli($host, $user, $password, $database);
 	if ($db_connection->connect_error) {
 		die($db_connection->connect_error);
 	}
 
 	/* Get likes/dislikes */
-	$query = "select likes, dislikes, name, views, artist from songs where url='{$url}'";
+	$query = "select likes, dislikes, name, views, artist, userid, songid from songs where url='{$url}'";
 			
 	/* Executing query */
 	$result = $db_connection->query($query);
@@ -21,7 +23,7 @@
 		/* Number of rows found */
 		$num_rows = $result->num_rows;
 		if ($num_rows === 0) {
-			echo "Empty Table<br>";
+			
 		} else {
 			for ($row_index = 0; $row_index < $num_rows; $row_index++) {
 				$result->data_seek($row_index);
@@ -29,8 +31,10 @@
 				$likes = $row['likes'];
 				$dislikes = $row['dislikes'];
 				$song_name = $row['name'];
+				$songID = $row['songid'];
 				$views = $row['views'];
 				$artist = $row['artist'];
+				$username = $row['userid'];
 			}
 		}
 	}
@@ -49,7 +53,7 @@
 			$user_comment .= $_POST['comment'];
 			$user_comment .= "</p>";
 			/* Get comments */
-			$query = "insert into comments values (1, 'test_user', '{$url}', 1, '{$comment_id}', '{$user_comment}', 12, 4)";
+			$query = "insert into comments values (1, 'test_user', '{$url}', {$songID}, '{$comment_id}', '{$user_comment}', 12, 4)";
 					
 			/* Executing query */
 			$result = $db_connection->query($query);
@@ -70,7 +74,7 @@
 		/* Number of rows found */
 		$num_rows = $result->num_rows;
 		if ($num_rows === 0) {
-			echo "Empty Table<br>";
+			
 		} else {
 			for ($row_index = 0; $row_index < $num_rows; $row_index++) {
 				$result->data_seek($row_index);
@@ -93,7 +97,7 @@
 					<link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
 			        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 			        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-			        <link rel="stylesheet" href="song_style.css" type="text/css">
+			        <link rel="stylesheet" href="resources/song_style.css" type="text/css">
 			        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 			        <script src="song.js"></script>
 				</head>
@@ -120,7 +124,7 @@
 							<h2>{$song_name}</h2>
 							<!-- Soundcloud Link -->
 							<iframe id="videoPlayer" width="100%" height="400" scrolling="no" frameborder="no" src="{$url}"></iframe>
-							<h4 class="inline">Uploaded By: </h4>
+							<h4 class="inline">Uploaded By: UserID</h4>
 							<h4 class="inline">{$username}</h4>
 
 							<div class="pull-right">
