756607718b445072cd4a66c55ac5814a4adf5c93
Arman <arman@umd.edu>
2017-05-08 09:56:34 -0400

    upload song and show song view - added nav bar to login pages

---
+++ affirmCreateUser.php
@@ -0,0 +1,115 @@
+<!doctype html>
+
+
+<?php 
+	require_once "userAuth.php";
+	
+	$db_connection = new mysqli($host, $user, $password, $database);
+	if ($db_connection->connect_error) {
+		die($db_connection->connect_error);
+	} else {
+		// echo "Connection to database established<br><br>";
+	}
+	
+	$username = $_POST['Username'];
+	$password = $_POST['Password'];
+	$query = "select * from users where name = '" . $username . "'";
+	
+	$result = $db_connection->query($query);
+	if (!$result) {
+		die("Retrieval failed: ". $db_connection->error);
+	} else {
+		/* Number of rows found */
+		$num_rows = $result->num_rows;
+		if ($num_rows === 0) {
+			$query2 = "select max(userid) as max from users";
+			$result2 = $db_connection->query($query2);
+			if (!$result2) {
+				die("Retrieval failed: ". $db_connection->error);
+			}
+			$assoc = $result2->fetch_assoc();
+			$increment = $assoc['max'] + 1;
+			$insert = "insert into users (name, userid, password) values ('$username', $increment, '$password')";
+			$insertion = $db_connection->query($insert);
+			if (!$insertion) {
+				die("Insertion failed: ". $db_connection->error);
+			}
+			
+			session_start();
+			$_SESSION['user'] = $username;
+			header("Location: main.html");
+		} else {
+			echo "<h1>Username already exists.  Please enter a different username.</h1>";
+			
+		}
+	}
+?> 
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Noteshare Login Page</title>
+		<link href="resources/css/bootstrap.min.css" rel="stylesheet">
+		<! <link rel="stylesheet" type="text/css" href="resources/style.css"/ > 
+	</head>
+	
+	<body>
+		    <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand"><strong><i id="logo">NoteShare</i></strong></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="main.html">Home</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="upload.php">Songs</a></li>
+                    <li><a href="createUser.php">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+	
+		<h1>Welcome to Noteshare!</h1>
+		
+		<h4>Login if you're already a member, or click Create Account<h4>
+		
+		<form class="form-horizontal" method=post action="affirmCreateUser.php">
+			<fieldset>
+				<div class="form-group">
+					<label class="col-md-4 control-label">Username:</label>
+					<div class="col-md-4">
+						<input id="title" name="Username" type="text" class="form-control input-md" required="">
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-md-4 control-label">Password:</label>
+					<div class="col-md-4">
+						<input id="title" name="Password" type="password" class="form-control input-md" required=""> </br>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="button"></label>
+					<div class="col-md-4 center-block">
+						<button type="submit" id="login" name="login" class="btn btn-primary center-block">Login</button>
+					</div>
+				</div>
+			</fieldset>
+		</form>
+		
+		<form class="form-horizontal" method=post action="createUser.php">
+			<div class="form-group">
+				<label class="col-md-4 control-label" for="button"></label>
+				<div class="col-md-4 center-block">
+					<button type="submit" id="create" name="create" class="btn btn-primary center-block">Create Account</button>
+				</div>
+			</div>
+		</form>
+		
+	</body>
+</html>
+			
---
+++ upload.php
@@ -0,0 +1,70 @@
+
+<html>
+
+<head>
+
+    </style>
+    <link rel="stylesheet" href="resources/stylesheet.css" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+</head>
+
+
+<body>
+
+    <nav class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand"><strong><i id="logo">NoteShare</i></strong></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="main.html">Home</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="upload.php">Songs</a></li>
+                    <li><a href="createUser.php">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+    <form class="form-horizontal" method=GET action="song.php">
+        <fieldset>
+            <div class="form-group">
+                <label class="col-md-4 control-label">Song Title</label>
+                <div class="col-md-4">
+                    <input id="title" name="title" type="text" class="form-control input-md" required="">
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label class="col-md-4 control-label">Artist Name</label>
+                <div class="col-md-4">
+                    <input id="artist" name="artist" type="text" class="form-control input-md" required="">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-4 control-label">URL</label>
+                <div class="col-md-4">
+                    <input id="url" name="url" type="text" class="form-control input-md" required="">
+                </div>
+            </div>
+
+
+            <div class="form-group">
+                <label class="col-md-4 control-label" for="button"></label>
+                <div class="col-md-4 center-block">
+                    <button id="button" name="button" class="btn btn-primary center-block">Submit</button>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+</body>
+
+</html>
