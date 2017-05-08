6b0dbfbcbb93b5c36c3591e12c48afe0be1eaede
Benson Sebastian <bseb96@gmail.com>
2017-05-08 17:13:51 -0400

    Update upload.php

--- final-project/upload.php
+++ final-project/upload.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
+    <link rel="stylesheet" type="text/css" href="resources/style.css" />
 </head>
 
 
