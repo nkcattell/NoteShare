1f4d729a611246c741e81103e6dde55d2d15e71a
Noah Cattell <noah.kristofer.cattell@gmail.com>
2017-05-08 13:45:39 -0400

    Update createUser.php

--- final-project/createUser.php
+++ final-project/createUser.php
@@ -7,10 +7,25 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Noteshare Create User Page</title>
 		<link href="resources/css/bootstrap.min.css" rel="stylesheet">
-		<! <link rel="stylesheet" type="text/css" href="resources/style.css"/ > 
+		<link rel="stylesheet" type="text/css" href="resources/style.css"/> 
 	</head>
 	
 	<body>
+		<nav class="navbar navbar-inverse navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand"><strong><i id="logo">NoteShare</i></strong></a>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="main.html">Home</a></li>
+						<li><a href="#">Profile</a></li>
+						<li><a href="#">Songs</a></li>
+                    	<li><a href="createUser.php">Register</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 		
 		<h1>Welcome to Noteshare!</h1>
 		
