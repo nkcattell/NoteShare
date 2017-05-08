a0bca26347b9d1779fb9b0cc66d9992908fb24af
Benson Sebastian <bseb96@gmail.com>
2017-05-08 16:56:53 -0400

    Update login.php

--- final-project/login.php
+++ final-project/login.php
@@ -18,9 +18,10 @@
 				</div>
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Profile</a></li>
-						<li><a href="#">Songs</a></li>
-                    				<li><a href="createUser.php">Register</a></li>
+						<li><a href="profile.php">Profile</a></li>
+						<li><a href="upload.php">Upload</a></li>
+						<li><a href="login.php">Login</a></li>
+						<li><a href="createUser.php">Register</a></li>
 					</ul>
 				</div>
 			</div>
