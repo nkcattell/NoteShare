018731878fecd48845446821fc663fe8e02a4f2a
Noah Cattell <noah.kristofer.cattell@gmail.com>
2017-05-08 17:18:40 -0400

    Add files via upload

--- final-project/song.php
+++ final-project/song.php
@@ -1,13 +1,44 @@
 <?php
 	require_once "userAuth.php";
 
-	$url = $_GET['url'];
 
-	/* Connecting to the database */		
 	$db_connection = new mysqli($host, $user, $password, $database);
 	if ($db_connection->connect_error) {
 		die($db_connection->connect_error);
 	}
+	
+	if (isset($_GET['url'])) {
+		$url = $_GET['url'];
+	} elseif (isset($_POST['songid'])) {
+		$query = "select url from songs where songid = {$_GET['songId']}";
+		if (!$result) {
+			die("Retrieval failed: ". $db_connection->error);
+		} else {
+			for ($row_index = 0; $row_index < $num_rows; $row_index++) {
+				$result->data_seek($row_index);
+				$row = $result->fetch_array(MYSQLI_ASSOC);
+				$url = $row['url'];
+			}
+		}
+	}
+	
+	$query = "select * from users where name='{$_SESSION['user']}'";
+	$result = $db_connection->query($query);
+	if (!$result) {
+		die("Retrieval failed: ". $db_connection->error);
+	} else {
+		for ($row_index = 0; $row_index < $num_rows; $row_index++) {
+				$result->data_seek($row_index);
+				$row = $result->fetch_array(MYSQLI_ASSOC);
+				$username = $row['name'];
+				$userid = $row['userid'];
+			}
+	}
+	
+	echo "<script>alert($username, $userid)</script>";
+
+	/* Connecting to the database */		
+
 	/* Get likes/dislikes */
 	$query = "select likes, dislikes, name, views, artist, userid, songid from songs where url='{$url}'";
 			
@@ -19,8 +50,8 @@
 		/* Number of rows found */
 		$num_rows = $result->num_rows;
 		if ($num_rows === 0) {
-			$sql = "INSERT INTO songs (artist, url, tags)
-			VALUES ('$url', '$url', '$url')";
+			$sql = "INSERT INTO songs (artist, url, tags, userid)
+			VALUES ('$url', '$url', '$url', '$userid')";
  			$db_connection->query($sql);
  			$likes = 0;
  			$dislikes = 0;
@@ -38,18 +69,31 @@
 			}
 		}
 	}
+	
+	$query = "select * from users where name='{$_SESSION['user']}'";
+	if (!$result) {
+		die("Retrieval failed: ". $db_connection->error);
+	} else {
+		for ($row_index = 0; $row_index < $num_rows; $row_index++) {
+				$result->data_seek($row_index);
+				$row = $result->fetch_array(MYSQLI_ASSOC);
+				$username = $row['name'];
+				$userid = $row['userid'];
+			}
+	}
+	
 	$views += 1;
 	$comment_id = $views;
 	$update_views = "update songs set views={$views} where url='{$url}'";
 	$result = $db_connection->query($update_views);
-	if (isset($_POST["submit"])) {
-		if (isset($_POST["comment"])) {
+	if (isset($_GET["submit"])) {
+		if (isset($_GET["comment"])) {
 			$user_comment = "<hr>
 								<h5>Username</h5><p>";
-			$user_comment .= $_POST['comment'];
+			$user_comment .= $_GET['comment'];
 			$user_comment .= "</p>";
 			/* Get comments */
-			$query = "insert into comments values (1, 'test_user', '{$url}', {$songID}, '{$comment_id}', '{$user_comment}', 12, 4)";
+			$query = "insert into comments values ('{$userid}', '{$username}', '{$url}', {$songID}, '{$comment_id}', '{$user_comment}', 0, 0)";
 					
 			/* Executing query */
 			$result = $db_connection->query($query);
@@ -127,13 +171,14 @@
 							<br>
 							<br>
 							<h4>Comments</h4>
-							<form action="{$_SERVER["PHP_SELF"]}" method="post">
+							<form action="song.php" method=get>
 								<div class="form-group">
 								    <input type="text" class="form-control input-lg" name="comment" id="input-lg" placeholder="Add a comment.."/>
 							    </div>
 								
 								<div class="pull-right">
 										<input class="btn btn-default" type="reset" value="Cancel" id="cancelButton">
+										<input type="hidden" name="url" value="{$url}">
 										<input type="submit" name="submit" class="btn btn-primary" value="Comment" id="commentButton"/>
 								</div>
 							</form>
